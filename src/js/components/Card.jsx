cee11cf8497baff4fac90c5825441b88b70cc0fd
Ferbarotz <ferbarotz23@gmail.com>
2025-06-05 21:27:48 +0000

    first commit

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 const Card = () => {
     return (
+        <div className="container mt-4 ">
+<div class="row ">
+  <div class="col-sm-3 mb-3 mb-sm-0">
+    <div class="card ">
+              <img src="https://img.pikbest.com/wp/202344/sleek-textured-background-featuring-dark-gray-plain-concrete_9935402.jpg!w700wp" class="card-img-top" alt="..."></img>
+      <div class="card-body ">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipsicing elit. Sapiente ese necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+        <img src="https://img.pikbest.com/wp/202344/sleek-textured-background-featuring-dark-gray-plain-concrete_9935402.jpg!w700wp" class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipsicing elit. Sapiente ese necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+</div>
+<div class="col-sm-3">
+    <div class="card">
+        <img src="https://img.pikbest.com/wp/202344/sleek-textured-background-featuring-dark-gray-plain-concrete_9935402.jpg!w700wp" class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipsicing elit. Sapiente ese necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+</div>
+<div class="col-sm-3">
+    <div class="card">
+        <img src="https://img.pikbest.com/wp/202344/sleek-textured-background-featuring-dark-gray-plain-concrete_9935402.jpg!w700wp" class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipsicing elit. Sapiente ese necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+</div>
 
- <div className="card card-body text-start mx-auto mt-1 card border-0 rounded-0 " style={{ width: "80rem"}}>
-
-        <div className="card mt-1" style={{width: "18rem"}}>
-            <img src="https://sdmntprnortheu.oaiusercontent.com/files/00000000-072c-61f4-8f29-3dc8df36322f/raw?se=2025-06-04T21%3A44%3A07Z&sp=r&sv=2024-08-04&sr=b&scid=e1f1111f-982a-5d89-99b7-86149e62ba3b&skoid=b928fb90-500a-412f-a661-1ece57a7c318&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-04T04%3A41%3A19Z&ske=2025-06-05T04%3A41%3A19Z&sks=b&skv=2024-08-04&sig=CpHP1yj1nQekDR1/8PCwHLFNnkJ6S0KTMCCaFZHhLl4%3D" className="card-img-top" alt="..."/>
-             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-             </div>
-        </div>
-    </div>    
-        
+</div>
+</div>
     );
     };
 
